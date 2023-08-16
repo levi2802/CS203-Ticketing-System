7fb8a1e42846d776fef9b0cf53ba6be91a5d65a7
Teo Keng Hiang <kenghiang3e42016@gmail.com>
2023-08-16 16:36:49 +0800

    Test

---
+++ src/pages/HomePage/Carousel.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+function MyCarousel() {
+    <Carousel>
+        <div>
+            <img src="images/image1.png" alt="" />
+        </div>
+    </Carousel>
+}
+
+export default MyCarousel;
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
+import MyCarousel from "./Carousel";
 
 function Home() {
     return (
         <>
             <Navbar/> 
+            <MyCarousel/>
         </>
     )
 }
