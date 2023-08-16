939a90029eaa3770b594a9157e36d8fe8c483bee
Teo Keng Hiang <kenghiang3e42016@gmail.com>
2023-08-16 20:43:37 +0800

    Merged with main branch

--- src/pages/HomePage/Navbar.jsx
+++ src/pages/HomePage/Navbar.jsx
@@ -6,15 +6,9 @@ function Navbar() {
     return (
         <nav>
             <ul className="nav-bar">
-<<<<<<< HEAD
-                <li className="nav-bar-name"> <Link to="/">TicketWarrior</Link> </li>
-                <li> <Link to="/login">Login</Link> </li>
-                <li> <Link to="/register">Register</Link> </li>
-=======
                 <li className="nav-bar-name">TicketWarrior</li>
                 <li><a href="#">Login</a></li>
                 <li><a href="#">Register</a></li>
->>>>>>> carousel
             </ul>
             <div className="nav-bar-line"></div>
         </nav>
