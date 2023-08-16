44619b06967acdd4d9d83063cc4f5b48e4a807f0
Teo Keng Hiang <kenghiang3e42016@gmail.com>
2023-08-16 16:58:07 +0800

    Added some sample images and carousel, remember to install react-responsive-carousel

--- src/pages/HomePage/Carousel.jsx
+++ src/pages/HomePage/Carousel.jsx
@@ -3,11 +3,22 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from 'react-responsive-carousel';
 
 function MyCarousel() {
-    <Carousel>
-        <div>
-            <img src="images/image1.png" alt="" />
-        </div>
-    </Carousel>
+    return (
+        <Carousel>
+            <div>
+                <img src="./images/image1.png" alt="" />
+                <p className="legend">Legend 1</p>
+            </div>
+            {/* <div>
+                <img src="images/image2.png" alt="" />
+                <p className="legend">Legend 2</p>
+            </div>
+            <div>
+                <img src="images/image3.png" alt="" />
+                <p className="legend">Legend 3</p>
+            </div> */}
+        </Carousel>
+    );
 }
 
 export default MyCarousel;
