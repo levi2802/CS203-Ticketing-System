d69ecf9c9e8d7474ef6737a2c99bb7f65d6863f4
Calvin Chua Yue Heng <calvin@Calvins-MacBook-Air.local>
2023-08-17 14:20:53 +0800

    Added minor changes to navbar links

--- src/pages/HomePage/Navbar.jsx
+++ src/pages/HomePage/Navbar.jsx
@@ -6,9 +6,9 @@ function Navbar() {
     return (
         <nav>
             <ul className="nav-bar">
-                <li className="nav-bar-name">TicketWarrior</li>
-                <li><a href="#">Login</a></li>
-                <li><a href="#">Register</a></li>
+                <li className="nav-bar-name"><Link to="/" style={{textDecoration: 'none'}}>TicketWarrior</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/register">Register</Link></li>
             </ul>
             <div className="nav-bar-line"></div>
         </nav>
