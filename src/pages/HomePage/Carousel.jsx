6f2f1f597543deca1b408827ab276d76a68f17bd
Calvin Chua Yue Heng <calvin@Calvins-MacBook-Air.local>
2023-08-20 21:05:27 +0800

    minor css changes

--- src/pages/HomePage/Carousel.jsx
+++ src/pages/HomePage/Carousel.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import { Link } from 'react-router-dom';
 import sample1 from "./images/image1.png";
-import sample2 from "./images/image2.png";
+import sample4 from "./images/image2.png";
 import sample3 from "./images/image3.png";
 
 
 function MyCarousel() {
     return (
         <Carousel className="carousel" autoPlay={true} showArrows={false} showIndicators={true} showStatus={false} showThumbs={false}>
-            <div>
+             <div>
                 <img src={sample1} alt="" style={{ height: "500px", width: "1050px" }} />
             </div>
             <div>
-                <img src={sample2} alt="" style={{ height: "500px", width: "1050px" }} />
+                <img src={sample4} alt="" style={{ height: "500px", width: "1050px" }} />
             </div>
             <div>
                 <img src={sample3} alt="" style={{ height: "500px", width: "1050px" }} />
