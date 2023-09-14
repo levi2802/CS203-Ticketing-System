17aad9883b507ecc22fcb7ea46a1868ecc3c3385
Caleb Ong Ye Sheng <caleb.ong.2022@scis.smu.edu.sg>
2023-09-14 22:12:36 +0800

    test-merge-1

--- TicketWarrior/src/main/java/com/cs203/TicketWarrior/Registration/exceptions/SeatNotAvailableException.java
+++ TicketWarrior/src/main/java/com/cs203/TicketWarrior/Registration/exceptions/SeatNotAvailableException.java
@@ -1,4 +1,4 @@
-package com.cs203.TicketWarrior.Registration.exceptions;
+package com.cs203.TicketWarrior.Registration.Exceptions;
 
 public class SeatNotAvailableException extends RuntimeException {
     public SeatNotAvailableException(String message) {
