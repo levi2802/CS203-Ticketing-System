01d690cab8bef8ef09ae616a6024aa527a74bec3
Loke Weien, Isaac <isaac.loke.2021@scis.smu.edu.sg>
2023-09-15 11:52:50 +0800

    User.java issue

--- TicketWarrior/src/main/java/com/cs203/TicketWarrior/Registration/models/User.java
+++ TicketWarrior/src/main/java/com/cs203/TicketWarrior/Registration/models/User.java
@@ -1,22 +1,15 @@
 package com.cs203.TicketWarrior.Registration.models;
 
-<<<<<<< HEAD
 import java.util.Collection;
 import java.util.List;
 
 import lombok.*;
-=======
-import java.util.HashSet;
-import java.util.Set;
-import java.util.List; // import the List class
->>>>>>> orderHistorySprint2
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-<<<<<<< HEAD
 
 @Data
 @Builder
@@ -25,17 +18,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 //@Entity
 @Document(collection = "users")
 public class User implements UserDetails {
-=======
-import jakarta.persistence.CascadeType; // import jakarta persistence classes
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.OneToMany;
-
-// This is the User Data Transfer Object. It is used to send all of the registration information to our Spring Backend.
-@Document(collection = "users")
-public class User {
->>>>>>> orderHistorySprint2
 
     @Id
     private String id;
@@ -46,24 +28,9 @@ public class User {
     @NonNull
     private String password;
 
-<<<<<<< HEAD
     // Declare as enum, using String to represent instead of integer
 //    @Enumerated(EnumType.STRING)
     private ERole role;
-=======
-    @DBRef
-    private Set<Role> roles = new HashSet<>();
-
-    // Users can have multiple orders.
-    // "user" is what is used in the Order class to map the orders to the user.
-    // orders cannot exist without their respective user.
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Order> orders;
-
-    public User() {
-
-    }
->>>>>>> orderHistorySprint2
 
     public User(String username, String password) {
         this.username = username;
@@ -94,29 +61,4 @@ public class User {
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role.name()));
     }
-<<<<<<< HEAD
 }
-=======
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
-
-    // Getters and setters for User Orders
-    public List<Order> getOrders() {
-        return orders;
-    }
-
-    public void setOrders(List<Order> orders) {
-        this.orders = orders;
-    }
-}
->>>>>>> orderHistorySprint2
