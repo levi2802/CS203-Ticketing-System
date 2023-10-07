3862aa5c4ffe05676263c603066d60eee8a723b2
CalvinChuaYH <calvinchuayh@gmail.com>
2023-10-07 19:33:35 +0800

    Fix purchase integration testing

--- TicketWarrior/src/test/java/com/cs203/TicketWarrior/Registration/IntegrationTests/PurchaseIntegrationTest.java
+++ TicketWarrior/src/test/java/com/cs203/TicketWarrior/Registration/IntegrationTests/PurchaseIntegrationTest.java
@@ -3,9 +3,13 @@ package com.cs203.TicketWarrior.Registration.IntegrationTests;
 import com.cs203.TicketWarrior.Registration.jwt.JwtService;
 import com.cs203.TicketWarrior.Registration.models.Purchase;
 import com.cs203.TicketWarrior.Registration.models.User;
+import com.cs203.TicketWarrior.Registration.payload.AuthenticationRequest;
+import com.cs203.TicketWarrior.Registration.payload.AuthenticationResponse;
 import com.cs203.TicketWarrior.Registration.repository.PurchaseRepository;
 import com.cs203.TicketWarrior.Registration.repository.UserRepository;
+import com.cs203.TicketWarrior.Registration.services.AuthenticationService;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
@@ -48,36 +52,59 @@ public class PurchaseIntegrationTest {
     private PasswordEncoder passwordEncoder;
 
     @Autowired
-    private JwtService jwtService;
+    private AuthenticationService authenticationService;
+
+    @BeforeEach
+    void setUp() {
+        purchaseRepository.deleteAll();
+        userRepository.deleteAll();
+    }
 
     @AfterEach
     void tearDown(){
         // clear the database after each test
         purchaseRepository.deleteAll();
+        userRepository.deleteAll();
     }
+
+
 
     @Test
     public void getPurchases_Success() throws Exception {
-        URI uri = new URI(baseUrl + port + "/api/purchases");
+        //Arrange
+        //Save user in DB
+//        User user = new User("calvin@gmail.com", "testCalvin", passwordEncoder.encode("goodpassword"));
+        AuthenticationRequest request = new AuthenticationRequest("testCalvin@gmail.com", "testCalvin", "goodpassword");
+        AuthenticationResponse response = authenticationService.register(request);
+//        userRepository.save(user);
+
+
+
+        //Set URI
+        String id = "testCalvin";
+        URI uri = new URI(baseUrl + port + "/api/purchases/" + id);
         List<String> seatIDs = new ArrayList<>();
         seatIDs.add("A1");
-        Purchase purchase = new Purchase("user1", "barbier1", seatIDs);
+        Purchase purchase = new Purchase("testCalvin", "barbier1", seatIDs);
         purchaseRepository.save(purchase);
 
-        // Need to use array with a ReponseEntity here
-        User user = new User("calvin@gmail.com", "calvin", passwordEncoder.encode("goodpassword"));
-        userRepository.save(user);
+        //Act
+        //Authenticate user
+//        AuthenticationRequest request = new AuthenticationRequest("testCalvin", "goodpassword");
+//        AuthenticationResponse response = authenticationService.authenticate(request);
 
-        String token = jwtService.generateToken(user);
-
+        //Set token in http header
+        String token = response.getToken();
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer " + token);
         HttpEntity<?> entity = new HttpEntity<>(headers);
 
+        //Call API
         ResponseEntity<Purchase[]> result = testRestTemplate.exchange(uri, HttpMethod.GET, entity, Purchase[].class);
+//        ResponseEntity<Purchase[]> result = testRestTemplate.getForEntity(uri, Purchase[].class);
         Purchase[] purchases = result.getBody();
 
-
+        //Assert
         assertEquals(200, result.getStatusCode().value());
         assertEquals(1, purchases.length);
     }
