bb1cd55c0ae8215ce172e6bc6fd6bdc65df68299
Loke Weien, Isaac <isaac.loke.2021@scis.smu.edu.sg>
2023-11-06 17:31:06 +0800

    Removed unused import

--- src/pages/TimingPage/TimingApp/Dates.jsx
+++ src/pages/TimingPage/TimingApp/Dates.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 function Dates({ selectedDate, setSelectedDate }) {
   // const [selectedDateInfo, setSelectedDateInfo] = useState(null);
